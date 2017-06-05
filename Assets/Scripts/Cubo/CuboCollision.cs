1d4c2f4c0fcd607cfccef2302777a886660cf666
Diogenes Ademir Domingos <k.fus@hotmail.com>
2017-06-05 01:19:52 -0300

    Fisrt commint Sand Waste Project

---
+++ Assets/Scripts/Cubo/CuboCollision.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuboCollision : MonoBehaviour {
+
+    bool filho;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+       bool b =  collision.gameObject.CompareTag("VERDE");
+
+        
+        if (b)
+        {
+            Debug.Log("teve colisão com:" + collision.gameObject.tag);
+
+           // collision.gameObject.transform.position = transform.position;
+            
+        }
+
+        
+    }
+
+    
+}
